e13732eb25cd3fb50a7180c6c1d2f12f1d1f3704
Peter-John88 <bailing.guo@gmail.com>
2018-04-03 15:30:06 +0800

    Create 14slicing.py

---
+++ 14slicing.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8   -*-
+
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print L[0:3]   #取前3个元素
+print L[:3]    #开始位置不写就表示是从0开始
+print L[1:3]
+
+print L[-2:]
+print L[-2:-1]    #-1表示最后一个元素
+
+
+
+
+
+L = range(100)
+print L
+print L[:10]   #前10个数
+print L[-10:]  #后10个数
+print L[10:20]
+print L[:10:2]
+print L[::5]
+print L[:]
+
+
+
+
+
+
+
+print (0, 1, 2, 3, 4, 5)[:3]
+print 'ABCDEFG'[:3]
+print 'ABCDEFG'[::2]
+
+
