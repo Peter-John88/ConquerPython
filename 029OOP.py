58aa27189a1c4b0b9d9dc55fd402457dd8761166
xiaoshu <bailing.guo@gmail.com>
2018-04-08 20:14:12 +0800

    create 029OOP.py

--- 029OOP.py
+++ 029OOP.py
@@ -24,7 +24,7 @@ class Student(object):
         self.name=name
         self.score=score
     def print_score(self):
-        print '%s: %s', %(self.name, self.score)
+        print '%s: %s' %(self.name, self.score)
 
     #给对象发消息实际上就是调用对象对应的关联函数，我们称之为对象的方法（Method）。面向对象的程序写出来就像这样：
 bart=Student('Bart Simpson', 59)
